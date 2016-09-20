3603c6e3d59f7bd031ca2aadb7b9b7a9580ac515
deeps-webonise <deepn94@gmail.com>
2016-09-20 12:08:31 +0530

    Modified file

--- src/main/java/com/webonise/collectionassignment/TestMain.java
+++ src/main/java/com/webonise/collectionassignment/TestMain.java
@@ -9,18 +9,26 @@ public class TestMain {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Press 1 to use ArrayList\n Press 2 for Hash Set\n Press 3 for Hash Map");
         int choice = scanner.nextInt();
-        if(choice == 1 ) {
-            ICollectionMenu collectionMenu = new ArrayListCollection(); //Type Compatibility
-            collectionMenu.useCollectionToDisplayData();
-        } else if(choice == 2) {
-            ICollectionMenu collectionMenu = new HashSetCollection(); //Type Compatibility
-            collectionMenu.useCollectionToDisplayData();
-        } else if (choice ==3){
-            ICollectionMenu collectionMenu = new HashMapCollection(); //Type Compatibility
-            collectionMenu.useCollectionToDisplayData();
-        } else {
-            System.out.println("Wrong choice");
-            return;
+        switch (choice) {
+            case 1:
+                ICollectionMenu arrayListCollection = new ArrayListCollection(); //Type Compatibility
+                arrayListCollection.useCollectionToDisplayData();
+                break;
+
+            case 2:
+                ICollectionMenu hashSetCollection = new HashSetCollection(); //Type Compatibility
+                hashSetCollection.useCollectionToDisplayData();
+                break;
+
+            case 3:
+                ICollectionMenu hashMapCollection = new HashMapCollection(); //Type Compatibility
+                hashMapCollection.useCollectionToDisplayData();
+                break;
+            
+
+            default:
+                System.out.println("Invalid Choice");
+                return;
         }
     }
 }
